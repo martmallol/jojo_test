c422917cf5ba3560e38146f73fb81711243d52d0
Martin Mallol <martinmallolcc@gmail.com>
2022-08-11 20:33:56 -0300

    Avanzo con logica

--- src/Components/pages/Quiz.js
+++ src/Components/pages/Quiz.js
@@ -1,34 +1,84 @@
 import React, {useState} from 'react';
 import questions from './questions';
+import './Quiz.css'
 
 function Quiz() {
   const [actualQuestion, setActualQuestion] = useState(0);
-  const [points, setPoints] = useState(0);
   const [isFinished, setIsFinished] = useState(false);
+  const [actualAnswer, setActualAnswer] = useState(-1);
+  let characters = {
+    jonathan: 0, 
+    joseph: 0,
+    jotaro: 0,
+    josuke: 0,
+    giorno: 0,
+    jolyne: 0};
 
-  //Creo handler para cuando se elige la respuesta
+  const updateCharactersValues = (elem) => {
+    console.log(characters);
+    const answer = questions[actualQuestion].options[elem];
+    // Por que no me cambia los valores?????
+    Object.keys(characters).forEach(key => {
+        characters[key] += answer[key];
+    });
+    console.log(characters);
+  };
+
+  const restartCharactersValues = (elem) => {};
+
+  const changeValue = () => {
+    characters.giorno = 7;
+  }
+
+  // Handler for choosing an option and clicking next page
+  const handleNextPage = () => {
+    if(actualAnswer != -1) {
+        updateCharactersValues(actualAnswer);
+        setActualAnswer(-1);
+        setActualQuestion(actualQuestion+1);
+    } else {
+        // Error, debes elegir una respuesta!!
+    }
+  };
+
+  // Handler for clicking previous page
+  const handlePreviousPage = () => {
+    setActualQuestion(actualQuestion-1);
+    setActualAnswer(-1)
+  };
+
+  // Handler for game's finishcharacters[key]
+  const handleGameFinish = () => {
+    if(actualAnswer != -1) {
+        updateCharactersValues(actualAnswer);
+        setIsFinished(true);
+    } else {
+        // Error, debes elegir una respuesta!!
+    }
+  };
 
   // What buttons should there be, depending on the question we are currently on
   const whichButtons = () => {
     if(actualQuestion == 0) {
-        return (<button>Next</button>)
+        return (<div><button onClick={handleNextPage}>Next</button></div>)
     } else if (actualQuestion == (questions.length - 1)) {
         return (
         <div>
-            <button>Back</button> 
-            <button>Get my JoJo</button>
+            <button onClick={handlePreviousPage}>Back</button> 
+            <button onClick={handleGameFinish}>Get my JoJo</button>
         </div>
         )
     } else {
         return (
         <div>
-            <button>Back</button> 
-            <button>Next</button>
+            <button onClick={handlePreviousPage}>Back</button> 
+            <button onClick={handleNextPage}>Next</button>
         </div>
         )
     }
   }
 
+  // Interface
   return (
     <main>
         <div className='question-number'>
@@ -39,11 +89,14 @@ function Quiz() {
         </div>
         <div className='answers'>
             {questions[actualQuestion].options.map((answer) => (
-                <button key='{answer.textAnswer'> {answer.textAnswer}</button>
+                <button onClick={() => {setActualAnswer(answer.id);}}> {answer.textAnswer}</button>
             ))}
         </div>
         <div className='back-next'>
             {whichButtons()}
+        </div>
+        <div className='comoEstanLasCosas(lotengoqueborrar)'>
+            {characters.giorno}
         </div>
     </main>
   )
--- src/Components/pages/questions.js
+++ src/Components/pages/questions.js
@@ -1,32 +1,41 @@
-let jonathan, joseph, jotaro, josuke, giorno, jolyne;
-jonathan = joseph = jotaro = josuke = giorno = jolyne = 0;
+//let jonathan, joseph, jotaro, josuke, giorno, jolyne;
+//jonathan = joseph = jotaro = josuke = giorno = jolyne = 0;
 
 const questions = [
     {
         title: 'Would you make a joke at a funeral?',
         options: [
-            {textAnswer: 'Yes', joseph: 3, josuke: 1, jolyne: 1},
-            {textAnswer: 'No', jonathan: 3, giorno: 3, jotaro: 1}
+            {id: 0, textAnswer: 'Yes', jonathan:0, joseph: 3, jotaro: 0, josuke: 1, giorno:0, jolyne: 1},
+            {id: 1, textAnswer: 'No', jonathan: 3, joseph: 0, jotaro: 1, josuke: 0, giorno: 3, jolyne: 0}
         ]
     },
     {
         title: 'Which is your favorite sport?',
         options: [
-            {textAnswer: 'Football', giorno: 3, jonathan: 1},
-            {textAnswer: 'Baseball', jolyne: 3, jotaro: 1, joseph: 1},
-            {textAnswer: 'Rugby', jonathan: 3},
-            {textAnswer: 'Videogames', josuke: 3, jotaro: 3}
+            {id: 0, textAnswer: 'Football', jonathan: 1, joseph:0, jotaro: 0, josuke:0, giorno: 3, jolyne: 0},
+            {id: 1, textAnswer: 'Baseball', jonathan: 0, joseph: 1, jotaro: 1, josuke: 0, giorno: 0, jolyne: 3},
+            {id: 2, textAnswer: 'Rugby', jonathan: 3, joseph: 0, jotaro: 0, josuke: 0, giorno: 0, jolyne: 0},
+            {id: 3, textAnswer: 'Videogames', jonathan: 0, joseph: 0, jotaro: 3, josuke: 3, giorno: 0, jolyne: 0 }
         ]
-    },
+    }/*,
     {
         title: 'What would you rather have for dinner?',
         options: [
-            {textAnswer: 'Squid Ink Spaghetti', joseph: 3, giorno: 1},
-            {textAnswer: 'Cigarettes and a Mojito', jotaro: 3},
-            {textAnswer: 'Caprese Salad', josuke: 3, giorno: 3},
-            {textAnswer: 'Hot Dog and Coca Cola', jolyne: 3, joseph: 1}
+            {id: 0, textAnswer: 'Squid Ink Spaghetti', joseph: 3, giorno: 1},
+            {id: 1, textAnswer: 'Cigarettes and a Mojito', jotaro: 3},
+            {id: 2, textAnswer: 'Caprese Salad', josuke: 3, giorno: 3},
+            {id: 3, textAnswer: 'Hot Dog and Coca Cola', jolyne: 3, joseph: 1}
         ]
-    }
+    },
+    {
+        title: 'What are your plans for the weekend?',
+        options: [
+            {id: 0, textAnswer: 'Look at yourself in the mirror for 8 straight hours', joseph: 3, giorno: 1},
+            {id: 1, textAnswer: 'Go to a poker tournament', jotaro: 3, josuke: 1, joseph: 1},
+            {id: 2, textAnswer: 'Wait eagerly until the hour of tea arrives', jonathan: 3},
+            {id: 3, textAnswer: 'Befriend with an alien', josuke: 3, jolyne: 3}
+        ]
+    }*/
 ];
 
 export default questions;
