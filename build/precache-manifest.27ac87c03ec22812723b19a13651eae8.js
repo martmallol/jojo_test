52e133329926ca26c9224df55692f95096e985a7
Martin Mallol <martinmallolcc@gmail.com>
2022-10-10 13:20:33 -0300

    Pre deploy

---
+++ build/precache-manifest.27ac87c03ec22812723b19a13651eae8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d0b098571d951290349",
+    "url": "/jojo_test/static/css/main.e1921ecb.chunk.css"
+  },
+  {
+    "revision": "2d0b098571d951290349",
+    "url": "/jojo_test/static/js/main.2d0b0985.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/jojo_test/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "0c464ce0a80efb75fd39",
+    "url": "/jojo_test/static/js/2.0c464ce0.chunk.js"
+  },
+  {
+    "revision": "67a27a8094da80758622",
+    "url": "/jojo_test/static/js/runtime~main.67a27a80.js"
+  },
+  {
+    "revision": "4964c070547e4ee20368045a9751bc76",
+    "url": "/jojo_test/static/media/jolyne.4964c070.jpg"
+  },
+  {
+    "revision": "b262042198c14ff441676e9344f660d9",
+    "url": "/jojo_test/static/media/jojo-logo.b2620421.jpg"
+  },
+  {
+    "revision": "9e2ee9882b2af2f0cfc65d34a1dbf00c",
+    "url": "/jojo_test/static/media/jojos.9e2ee988.jpg"
+  },
+  {
+    "revision": "18a736a670717d05f016511b0c221e67",
+    "url": "/jojo_test/static/media/funeral.18a736a6.jpg"
+  },
+  {
+    "revision": "b438de7fc299bb83d59cd6f3b5fa6f6a",
+    "url": "/jojo_test/static/media/baseball.b438de7f.jpeg"
+  },
+  {
+    "revision": "ba88d972ae3768a037ae4be9b9b432a8",
+    "url": "/jojo_test/static/media/caprese.ba88d972.png"
+  },
+  {
+    "revision": "c91686d2753ac3bf368ab366461ecbe1",
+    "url": "/jojo_test/static/media/poker.c91686d2.jpg"
+  },
+  {
+    "revision": "35879f2f45d03023d5dfc3d63861a473",
+    "url": "/jojo_test/static/media/jonathan.35879f2f.jpg"
+  },
+  {
+    "revision": "d4b7a300cac8dfdb3ce4ae080d86ae58",
+    "url": "/jojo_test/static/media/joseph.d4b7a300.jpg"
+  },
+  {
+    "revision": "06747356d881b48a71eff11f39f0ecd1",
+    "url": "/jojo_test/static/media/jotaro.06747356.jpg"
+  },
+  {
+    "revision": "681065ae299247f3dd684303dab9246a",
+    "url": "/jojo_test/static/media/josuke.681065ae.jpg"
+  },
+  {
+    "revision": "6c897d985cf60469641a514ac4ab2f43",
+    "url": "/jojo_test/static/media/giorno.6c897d98.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/jojo_test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "db3fe37b30e7a415838dadab07f96aaa",
+    "url": "/jojo_test/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/jojo_test/precache-manifest.27ac87c03ec22812723b19a13651eae8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/jojo_test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
