6ef27b3a9e20e3f2dc648c891c33c6d63d4307b7
Martin Mallol <martinmallolcc@gmail.com>
2022-10-10 14:20:31 -0300

    Updates

---
+++ precache-manifest.44c3faf748f7ad488b27780fd32eb03f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c5b48301d4713d942d7",
+    "url": "/jojo_test/static/css/main.f3e0737f.chunk.css"
+  },
+  {
+    "revision": "3c5b48301d4713d942d7",
+    "url": "/jojo_test/static/js/main.a1795268.chunk.js"
+  },
+  {
+    "revision": "949af10a2f7137a2afdf",
+    "url": "/jojo_test/static/js/runtime~main.5c7c25cc.js"
+  },
+  {
+    "revision": "554d7149c6d28e89b3c0",
+    "url": "/jojo_test/static/js/2.3b2e347f.chunk.js"
+  },
+  {
+    "revision": "60cf4ecf76dd825d6b0e",
+    "url": "/jojo_test/static/js/3.b84871a5.chunk.js"
+  },
+  {
+    "revision": "4964c070547e4ee20368045a9751bc76",
+    "url": "/jojo_test/static/media/jolyne.4964c070.jpg"
+  },
+  {
+    "revision": "b262042198c14ff441676e9344f660d9",
+    "url": "/jojo_test/static/media/jojo-logo.b2620421.jpg"
+  },
+  {
+    "revision": "9e2ee9882b2af2f0cfc65d34a1dbf00c",
+    "url": "/jojo_test/static/media/jojos.9e2ee988.jpg"
+  },
+  {
+    "revision": "18a736a670717d05f016511b0c221e67",
+    "url": "/jojo_test/static/media/funeral.18a736a6.jpg"
+  },
+  {
+    "revision": "b438de7fc299bb83d59cd6f3b5fa6f6a",
+    "url": "/jojo_test/static/media/baseball.b438de7f.jpeg"
+  },
+  {
+    "revision": "ba88d972ae3768a037ae4be9b9b432a8",
+    "url": "/jojo_test/static/media/caprese.ba88d972.png"
+  },
+  {
+    "revision": "c91686d2753ac3bf368ab366461ecbe1",
+    "url": "/jojo_test/static/media/poker.c91686d2.jpg"
+  },
+  {
+    "revision": "35879f2f45d03023d5dfc3d63861a473",
+    "url": "/jojo_test/static/media/jonathan.35879f2f.jpg"
+  },
+  {
+    "revision": "d4b7a300cac8dfdb3ce4ae080d86ae58",
+    "url": "/jojo_test/static/media/joseph.d4b7a300.jpg"
+  },
+  {
+    "revision": "06747356d881b48a71eff11f39f0ecd1",
+    "url": "/jojo_test/static/media/jotaro.06747356.jpg"
+  },
+  {
+    "revision": "681065ae299247f3dd684303dab9246a",
+    "url": "/jojo_test/static/media/josuke.681065ae.jpg"
+  },
+  {
+    "revision": "6c897d985cf60469641a514ac4ab2f43",
+    "url": "/jojo_test/static/media/giorno.6c897d98.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/jojo_test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "886f1f931b1179c400d7cb08422b1715",
+    "url": "/jojo_test/index.html"
+  }
+];
