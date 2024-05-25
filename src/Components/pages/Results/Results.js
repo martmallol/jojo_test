889dc87e33c7cca777f1952c26bcefc76ac3cd7e
Martin <martinmallolcc@gmail.com>
2024-05-25 13:22:28 -0300

    REFACTOR: Results page

--- src/Components/pages/Results/Results.js
+++ src/Components/pages/Results/Results.js
@@ -2,7 +2,7 @@
 import './Results.css';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import charactersInfo from '../../../data/characters';
+import ResultsTable from '../../Results/ResultsTable/ResultsTable.js';
 const Results = () => {
   const [results, setResults] = useState([]);
 
@@ -18,34 +18,7 @@ const Results = () => {
   return (
     <section className="results">
       {results.length !== 0 ? (
-        <>
-          <div>
-            <h1>Check your past results!</h1>
-          </div>
-          <div className="results-container">
-            {results.map((result, idx) => {
-              return (
-                <div className="result" key={idx}>
-                  <div className="result--img">
-                    <img src={charactersInfo[result.jojo].profileImage}></img>
-                  </div>
-                  <div className="result--text">
-                    <span className="result--name">
-                      <h3>{`${result.name} is...`}</h3>
-                      <h2>{`${result.jojo}!`}</h2>
-                    </span>
-                    <h4>{result.date}</h4>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="play-again">
-            <Link to="/quiz" className="button">
-              {'Play Again'}
-            </Link>
-          </div>
-        </>
+        <ResultsTable results={results} />
       ) : (
         <>
           <div>
